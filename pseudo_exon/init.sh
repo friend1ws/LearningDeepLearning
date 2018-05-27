da5eb80bf1516caae3b6a4ddf52e4c47257a8981
friend1ws <friend1ws@gmail.com>
2018-05-27 16:59:54 +0000

    add init.sh

---
+++ pseudo_exon/init.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/env bash
+
+mkdir bin
+mkdir db
+mkdir script
+mkdir data
+
+sudo apt-get update
+sudo apt-get install python-pip
+sudo apt-get install zlib1g-dev
+sudo apt-get install libbz2-dev
+sudo apt-get install liblzma-dev
+sudo apt-get install bedtools
+pip install annot_utils
+
+WORKDIR=`pwd`
+
+# install htslib
+cd bin
+wget https://github.com/samtools/htslib/releases/download/1.8/htslib-1.8.tar.bz2
+tar jxvf htslib-1.8.tar.bz2 
+cd htslib-1.8/
+sudo ./configure 
+sudo make
+sudo make install
+cd $WORKDIR
+
+# install maxentscan
+cd bin
+wget wget http://genes.mit.edu/burgelab/maxent/download/fordownload.tar.gz
+tar zxvf fordownload.tar.gz
+mv fordownload maxent
+export PATH=~/bin/maxent:$PATH
+cd $WORKDIR
+
+# prepare data
+cd db
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_28/GRCh38.primary_assembly.genome.fa.gz
+gunzip GRCh38.primary_assembly.genome.fa.gz
+bash prep_exon.sh
+cd $WORKDIR
+
+
